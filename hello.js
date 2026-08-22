f715ff496038d33443d175ec70c3a1ef34507276
andikapry <andika.priyadiputra@gmail.com>
2026-08-23 06:52:28 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,13 +1,1 @@
-// Simple Hello World program
-console.log("Hello, World!");
-
-// You can also add variables and functions
-const name = "JavaScript";
-console.log(`Welcome to ${name}!`);
-
-// Simple function example
-function greet(person) {
-  return `Hello, ${person}! Welcome to JavaScript.`;
-}
-
-console.log(greet("Andika"));
+console.log ("andika");
